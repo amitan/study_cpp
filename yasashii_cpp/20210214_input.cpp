17e1ddd28c48ed0abe79a13b284c818533462d03
Ayumi Otomo <ayumi.otomo@dena.com>
2021-02-15 01:15:41 +0900

    プログラム追加

---
+++ yasashii_cpp/20210214_input.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int x;
+    cout << "整数を入力してください\n";
+    cin >> x;
+    cout << x << "が入力されました\n";
+}
---
+++ yasashii_cpp/20210214_input2.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int x1;
+    int x2;
+    cout << "整数を2つ入力してください\n";
+    cin >> x1 >> x2;
+    cout << "最初に" << x1 << "が入力されました\n";
+    cout << "次に" << x2 << "が入力されました\n";
+}
